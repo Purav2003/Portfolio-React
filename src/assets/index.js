42d3aaab1406ac9e7763289c638289a2900b681b
Purav <puravshah20@gnu.ac.in>
2023-07-22 11:06:06 +0530

    Updated logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./logo.jpeg";
+import logo1 from "./logo1.jpg"
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -93,5 +94,6 @@ export {
   link,
   pythoncerti,
   sqlcerti,
-  bigdata101certi
+  bigdata101certi,
+  logo1
 };
